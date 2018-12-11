eab206887d65cef27fd252e67436e0cfc1991a0f
alex.wang <alex.wang@centrality.ai>
2018-12-11 16:01:09 +1300

    save

---
+++ ci/deployment/publish.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+
+NEW_SSH_RSA_FILE_PATH=~/.ssh/id_rsa
+
+# dir holding this script
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+# prevent jenkins rebuild
+LAST_COMMIT_AUTHOR=$(git log -1 --pretty=format:'%an');
+
+if [ "$LAST_COMMIT_AUTHOR" = "$GIT_NAME" ]; then
+  echo "Terminating build started by Jenkins Git push";
+  exit 0;
+fi
+
+# set shell to verbose, instant exit mode
+set -ex
+
+# Required Environment variables. Set them by using the commands below
+: "${IMAGE_NAME:?IMAGE_NAME environment variable is required}"
+: "${GIT_NAME:?GIT_NAME environment variable is required}"
+: "${GIT_EMAIL:?GIT_EMAIL environment variable is required}"
+: "${GIT_BRANCH:?GIT_BRANCH environment variable is required}"
+: "${GEMFURY_TOKEN:?GEMFURY_TOKEN environment variable is required}"
+: "${GEMFURY_EXTERNAL_TOKEN:?GEMFURY_EXTERNAL_TOKEN environment variable is required}"
+
+cp $NEW_SSH_RSA_FILE_PATH ./git-ssh-key
+
+docker build \
+  -t "$IMAGE_NAME" \
+  --build-arg GIT_NAME="$GIT_NAME" \
+  --build-arg GIT_EMAIL="$GIT_EMAIL" \
+  --build-arg GIT_BRANCH="$GIT_BRANCH" \
+  --build-arg GEMFURY_TOKEN="$GEMFURY_TOKEN" \
+  --build-arg GEMFURY_EXTERNAL_TOKEN="$GEMFURY_EXTERNAL_TOKEN" \
+  -f $DIR/Dockerfile .
