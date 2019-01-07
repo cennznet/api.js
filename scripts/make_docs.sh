4905389b804aa5a218da3ea249a7522aa3aa31b8
Alex Wang <alex.wang@centrality.ai>
2019-01-07 22:19:57 +0000

    Merged in DK-28-fix-changelog (pull request #19)
    chore: generate changeLog by conventional-changelog

--- scripts/make_docs.sh
+++ scripts/make_docs.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+npm install -g conventional-changelog
+conventional-changelog -p angular -i CHANGELOG.md -s -r 0 -l
 apt-get update  -y
 apt-get  install python-dev -y
 curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
