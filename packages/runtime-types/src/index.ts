ea7a8b3428d83845cdc10d94ac4cb8b5293a0368
Shaopeng Wang(Shaun) <seanwangsp@gmail.com>
2019-01-18 04:20:02 +0000

    feat: Add new runtime type 'AssetOptions'.
    Merged in feature/asset-options-type (pull request #25)
    
    Approved-by: Ian He <ian-he@outlook.com>

--- packages/runtime-types/src/index.ts
+++ packages/runtime-types/src/index.ts
@@ -1,2 +1,3 @@
 export {default as AssetId} from './types/AssetId';
 export {default as Decimals} from './types/Decimals';
+export {default as AssetOptions} from './types/AssetOptions';
---
+++ packages/runtime-types/src/types/AssetOptions.ts
@@ -0,0 +1,14 @@
+/*
+ Custom `AssetOptions` type for generic asset module.
+*/
+
+import Balance from '@polkadot/types/Balance';
+import Struct from '@polkadot/types/codec/Struct';
+
+class AssetOptions extends Struct {
+  constructor(value: any) {
+    super({total_supply: Balance}, value);
+  }
+}
+
+export default AssetOptions;
