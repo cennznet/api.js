568fe09c380b19991ff0379246c6416becfafea4
ian.he <ian.he@centrality.ai>
2019-02-04 14:24:50 +1300

    fix: export Topic and Value in runtime-types

--- packages/runtime-types/src/index.ts
+++ packages/runtime-types/src/index.ts
@@ -1,3 +1,5 @@
 export {default as AssetId} from './types/AssetId';
 export {default as Decimals} from './types/Decimals';
 export {default as AssetOptions} from './types/AssetOptions';
+export {default as Topic} from './types/Topic';
+export {default as Value} from './types/Value';
