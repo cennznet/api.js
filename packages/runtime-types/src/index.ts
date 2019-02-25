92edd99f3f2cc703c2122c202e74222a614e9f2a
Alex Wang <alex.wang@centrality.ai>
2019-02-25 02:31:51 +0000

    feat: add runtime type `Amount`
    Merged in addTypeAmount (pull request #45)
    
    Approved-by: Ian He <ian-he@outlook.com>

--- packages/runtime-types/src/index.ts
+++ packages/runtime-types/src/index.ts
@@ -3,3 +3,4 @@ export {default as Decimals} from './types/Decimals';
 export {default as AssetOptions} from './types/AssetOptions';
 export {default as Topic} from './types/Topic';
 export {default as Value} from './types/Value';
+export {default as Amount} from './types/Amount';
---
+++ packages/runtime-types/src/types/Amount.ts
@@ -0,0 +1,8 @@
+/*
+ Custom Amount type, implementation of u128 found in
+ @polkadot/types
+*/
+
+import {u128} from '@polkadot/types';
+
+export default u128;
