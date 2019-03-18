ee97b0e8b1deb8c69fd26e14bfe8e737b89a392d
ian.he <ian.he@centrality.ai>
2019-03-18 14:34:10 +1300

    feat: put generateStorageDoubleMapKey in @cennznet/util

--- packages/util/src/index.ts
+++ packages/util/src/index.ts
@@ -2,3 +2,5 @@ export {default as Keyring} from '@polkadot/keyring';
 export {default as TestingPairs} from '@polkadot/keyring/testingPairs';
 export * from '@polkadot/util';
 export * from '@polkadot/util-crypto';
+
+export {generateStorageDoubleMapKey} from './storage/doubleMap';
---
+++ packages/util/src/storage/doubleMap.ts
@@ -0,0 +1,24 @@
+import {Codec} from '@polkadot/types/types';
+import {stringToU8a, u8aToHex} from '@polkadot/util';
+import {xxhashAsHex} from '@polkadot/util-crypto';
+
+/**
+ * Generate the key of a double map storage
+ * @param prefixString
+ * @param key1
+ * @param key2
+ */
+export function generateStorageDoubleMapKey(prefixString: string, key1: Codec, key2: Codec): string {
+    // key1 encoded
+    const prefixU8a: Uint8Array = stringToU8a(prefixString);
+    const key1U8a: Uint8Array = key1.toU8a(true);
+    const key1Encoded: Uint8Array = new Uint8Array(prefixU8a.length + key1U8a.length);
+    key1Encoded.set(prefixU8a);
+    key1Encoded.set(key1U8a, prefixU8a.length);
+    // key2 encoded
+    const length = 2;
+    const key2Encoded: string = u8aToHex(key2.toU8a(true)).substr(length);
+    const bitLength: number = 128;
+
+    return xxhashAsHex(key1Encoded, bitLength) + key2Encoded;
+}
