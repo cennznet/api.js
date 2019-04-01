2ad4160cf65ba792637ca17a5e085121bdcaf48b
karishma <bothara.karishma@gmail.com>
2019-04-01 14:37:18 +1300

    as per comments

---
+++ ci/deployment/buildToPublish.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+# dir holding this script
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+# Required Environment variables. Set them by using the commands below
+: "${IMAGE_NAME:?IMAGE_NAME environment variable is required}"
+: "${GIT_NAME:?GIT_NAME environment variable is required}"
+: "${GIT_EMAIL:?GIT_EMAIL environment variable is required}"
+
+NEW_SSH_RSA_FILE_PATH=~/.ssh/id_rsa
+
+# prevent jenkins rebuild
+LAST_COMMIT_AUTHOR=$(git log -1 --pretty=format:'%an');
+
+if [ "$LAST_COMMIT_AUTHOR" = "$GIT_NAME" ]; then
+  echo "Terminating build started by Jenkins Git push";
+  exit 0;
+fi
+
+# set shell to verbose, instant exit mode
+set -ex
+
+cp $NEW_SSH_RSA_FILE_PATH ./git-ssh-key
+
+docker build \
+  -t "$IMAGE_NAME" \
+  --build-arg GIT_NAME="$GIT_NAME" \
+  --build-arg GIT_EMAIL="$GIT_EMAIL" \
+  -f $DIR/../pr/DockerfileToPublish .
