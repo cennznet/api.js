039466758ad4b9cc0935734a70653c6318c9cbe5
karishma <bothara.karishma@gmail.com>
2019-04-02 14:05:55 +1300

    split build and bump into two stages

--- ci/deployment/bumpVersion.sh
+++ ci/deployment/bumpVersion.sh
@@ -7,6 +7,7 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 : "${IMAGE_NAME:?IMAGE_NAME environment variable is required}"
 : "${GIT_NAME:?GIT_NAME environment variable is required}"
 : "${GIT_EMAIL:?GIT_EMAIL environment variable is required}"
+: "${RELEASE_SCOPE:?RELEASE_SCOPE is required}"
 
 NEW_SSH_RSA_FILE_PATH=~/.ssh/id_rsa
 
@@ -27,4 +28,5 @@ docker build \
   -t "$IMAGE_NAME" \
   --build-arg GIT_NAME="$GIT_NAME" \
   --build-arg GIT_EMAIL="$GIT_EMAIL" \
-  -f $DIR/../pr/Dockerfile .
+  --build-arg RELEASE_SCOPE="$RELEASE_SCOPE" \
+  -f $DIR/Dockerfile .
