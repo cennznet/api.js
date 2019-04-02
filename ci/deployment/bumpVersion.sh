4c5dc5dc4ac68da98bcf47395bda4de2cc48107b
karishma <bothara.karishma@gmail.com>
2019-04-02 14:13:59 +1300

    fix

--- ci/deployment/bumpVersion.sh
+++ ci/deployment/bumpVersion.sh
@@ -28,5 +28,6 @@ docker build \
   -t "$IMAGE_NAME" \
   --build-arg GIT_NAME="$GIT_NAME" \
   --build-arg GIT_EMAIL="$GIT_EMAIL" \
+  --build-arg IMAGE_NAME="$IMAGE_NAME" \
   --build-arg RELEASE_SCOPE="$RELEASE_SCOPE" \
-  -f $DIR/Dockerfile .
+  -f $DIR/DockerfileToBumpVesion .
