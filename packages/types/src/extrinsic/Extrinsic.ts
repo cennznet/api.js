6248afe2e191b533b552f85de562b8c755fa0c48
Ian He <ian-he@outlook.com>
2019-04-11 23:25:50 +0000

    fix: missing deps
    Merged in fix_deps (pull request #71)
    
    Approved-by: Alex Wang <alex.wang@centrality.ai>

--- packages/types/src/extrinsic/Extrinsic.ts
+++ packages/types/src/extrinsic/Extrinsic.ts
@@ -6,10 +6,10 @@ import FeeExchange, {OptionalFeeExchange} from '@cennznet/types/extrinsic/FeeExc
 import {KeyringPair} from '@polkadot/keyring/types';
 import {Address, Compact, Hash, Method, Option, Signature, Struct} from '@polkadot/types';
 import {AnyNumber, AnyU8a, ArgsDef, Codec, IExtrinsic, SignatureOptions} from '@polkadot/types/types';
-import {isHex, isU8a, u8aToHex, u8aToU8a} from '@polkadot/util';
+import {assert, isHex, isU8a, u8aToHex, u8aToU8a} from '@polkadot/util';
 
-import {assert, blake2AsU8a} from '@cennznet/util';
 import {FunctionMetadata} from '@polkadot/types/Metadata/v0/Modules';
+import {blake2AsU8a} from '@polkadot/util-crypto';
 import {Certificate, Doughnut, OptionalDoughnut} from './Doughnut';
 import ExtrinsicSignature, {checkDoughnut, checkFeeExchange} from './ExtrinsicSignature';
 
