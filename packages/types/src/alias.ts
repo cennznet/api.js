84497e981efdfbe684b2522f445138d0514fb5d2
Alex Wang <alex.wang@centrality.ai>
2019-04-17 16:03:54 +1200

    feat: adding alias

--- packages/types/src/alias.ts
+++ packages/types/src/alias.ts
@@ -1,8 +1,8 @@
 export default {
-    "T::AccountId": "AccountId",
+    'T::AccountId': 'AccountId',
     // TODO: should not throw error
-    "ExchangeKey<T>": "ExchangeKey",
-    "T::AssetId": "AssetId",
-    "T::Balance": "Balance",
-    "ed25519::Signature": "Ed25519Signature"
+    'ExchangeKey<T>': 'ExchangeKey',
+    'T::AssetId': 'AssetId',
+    'T::Balance': 'Balance',
+    'ed25519::Signature': 'H512',
 };
