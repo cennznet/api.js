b968af9cec9c5bc89bb6c80fe8486f17188c1010
Alex Wang <alex.wang@centrality.ai>
2019-04-17 16:04:13 +1200

    feat: adding add from uri

--- packages/wallet/src/keyrings/SimpleKeyring.ts
+++ packages/wallet/src/keyrings/SimpleKeyring.ts
@@ -86,6 +86,10 @@ export class SimpleKeyring implements IKeyring<SerializedSimpleKeyring> {
         return this._keyring.addFromSeed(seed, meta, type);
     }
 
+    addFromUri(suri: string, meta: KeyringPair$Meta = {}, type: KeypairType = DEFAULT_KEYRING_TYPE): KeyringPair {
+        return this._keyring.addFromUri(suri, meta, type);
+    }
+
     private _deserialize(data: SerializedSimpleKeyring): void {
         this._keyring = new Keyring({type: DEFAULT_KEYRING_TYPE});
         for (const json of data) {
