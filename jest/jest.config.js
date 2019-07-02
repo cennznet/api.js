a6c874e6596404f2653749964a9de2cfa487cd31
ian.he <ian.he@centrality.ai>
2019-07-02 14:15:11 +1200

    chore: move @cennznet/wallet out of the repo

--- jest/jest.config.js
+++ jest/jest.config.js
@@ -6,31 +6,16 @@ module.exports = {
     },
     testMatch: ['/**/?(*.)+(spec|e2e).[jt]s?(x)'],
     testEnvironment: 'node',
-    collectCoverageFrom: [
-        'packages/wallet/src/**/*.[jt]s?(x)',
-        '!**/node_modules/**'
-    ],
-    coverageReporters: ['json', 'html'],
-    coverageThreshold: {
-        global: {
-            branches: 60,
-            functions: 80,
-            lines: 80,
-            statements: -10,
-        },
-    },
     testEnvironment: './jest/env.js',
     moduleNameMapper: {
         '@cennznet/api(.*)$': '<rootDir>/packages/api/src/$1',
         '@cennznet/types(.*)$': '<rootDir>/packages/types/src/$1',
-        '@cennznet/util(.*)$': '<rootDir>/packages/util/src/$1',
-        '@cennznet/wallet(.*)$': '<rootDir>/packages/wallet/src/$1'
+        '@cennznet/util(.*)$': '<rootDir>/packages/util/src/$1'
     },
     modulePathIgnorePatterns: [
         '<rootDir>/packages/api/build',
         '<rootDir>/packages/types/build',
         '<rootDir>/packages/util/build',
-        '<rootDir>/packages/wallet/build',
     ],
     setupFilesAfterEnv: ['./jest/jest.setup.js']
 };
