a1d9464ddfc9491c5b5c319ddb93738030332ff5
karishma <bothara.karishma@gmail.com>
2019-10-24 10:57:11 +1300

    adding copyright

--- packages/api/src/submittable/createSubmittable.ts
+++ packages/api/src/submittable/createSubmittable.ts
@@ -1,5 +1,19 @@
 /* eslint-disable no-dupe-class-members */
-// Copyright 2017-2019 @polkadot/api authors & contributors
+// Copyright 2019 Centrality Investments Limited
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// Copyright 2017-2018 @polkadot/api authors & contributors
 // This software may be modified and distributed under the terms
 // of the Apache-2.0 license. See the LICENSE file for details.
 
@@ -8,8 +22,6 @@ import {Constructor} from '@plugnet/types/types';
 import {ApiInterfaceRx, ApiTypes} from '../types';
 import {SubmittableExtrinsic} from './types';
 
-//import ApiBase from '../base1';
-import {ApiRx} from '@cennznet/api';
 import ApiBase from '@plugnet/api/base';
 
 type Creator<ApiType> = (extrinsic: Call | Uint8Array | string) => SubmittableExtrinsic<ApiType>;
