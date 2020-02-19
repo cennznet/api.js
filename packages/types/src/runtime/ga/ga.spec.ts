c346089b95bd747327bdbe97c8190e68883eabec
karishma <bothara.karishma@gmail.com>
2020-02-19 16:18:37 +1300

    Clean up

--- packages/types/src/runtime/ga/ga.spec.ts
+++ packages/types/src/runtime/ga/ga.spec.ts
@@ -12,7 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-import '@polkadot/types/injector';
 import {createTypeUnsafe, TypeRegistry, u32} from '@polkadot/types';
 
 import * as gaTypes from './';
--- packages/types/src/runtime/index.ts
+++ packages/types/src/runtime/index.ts
@@ -18,7 +18,6 @@ export * from './ga';
 export * from './rewards';
 export * from './sylo';
 export * from './transaction-payment';
-// export * from './fees';
 
 // The CENNZnet nonce type
 export {u64 as Index} from '@polkadot/types';
