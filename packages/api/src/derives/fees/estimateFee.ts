8413b8ba8672ad6384aa55675f95d91194fee2d2
karishma <bothara.karishma@gmail.com>
2020-03-17 13:47:54 +1300

    as per review comments

--- packages/api/src/derives/fees/estimateFee.ts
+++ packages/api/src/derives/fees/estimateFee.ts
@@ -1,5 +1,4 @@
 import {ApiInterfaceRx} from '@cennznet/api/types';
-import {DEFAULT_FEE_ASSET} from '@cennznet/types/extrinsic/constants';
 import Extrinsic from '@cennznet/types/extrinsic/Extrinsic';
 import {ChargeTransactionPayment} from '@cennznet/types/runtime/transaction-payment';
 import {AnyAssetId, IExtrinsic} from '@cennznet/types/types';
@@ -13,43 +12,49 @@ import {combineLatest, Observable, of} from 'rxjs';
 import {catchError, first, map, switchMap} from 'rxjs/operators';
 
 export function estimateFee(api: ApiInterfaceRx) {
-  return (
-    extrinsic: IExtrinsic,
-    feeAssetId: AnyAssetId,
-    sender: Address,
-    transactionPayment: ChargeTransactionPayment
-  ): Observable<any> => {
+  /* To estimate fee correctly using extrinsic's fake signature */
+  return (extrinsic: IExtrinsic, userFeeAssetId: AnyAssetId, maxPayment?: string): Observable<any> => {
     return combineLatest([
       api.rpc.state.getRuntimeVersion(),
       api.rpc.chain.getBlockHash(),
       api.rpc.chain.getBlockHash(0),
-      api.query.system.accountNonce(sender),
+      api.query.genericAsset.spendingAssetId(),
     ]).pipe(
       first(),
       switchMap(
-        ([runtimeVersion, blockHash, genesisHash, nonce]): Observable<RuntimeDispatchInfo> => {
+        ([runtimeVersion, blockHash, genesisHash, networkFeeAssetId]): Observable<[RuntimeDispatchInfo, any]> => {
           const registry = new TypeRegistry();
           const era = new ExtrinsicEra(registry, {
             current: 1,
             period: 1,
           });
-          (extrinsic as Extrinsic).signFake(sender, {
-            runtimeVersion,
-            era,
-            blockHash,
-            genesisHash,
-            nonce: nonce.toString(),
-            transactionPayment,
-          });
-          return api.rpc.payment.queryInfo(extrinsic.toHex());
+          const nonce = null;
+          const sender = '5GrwvaEF5zXb26Fz9rcQpDWS57CtERHpNehXCPcNoHGKutQY';
+          const feeExchange = {
+            assetId: userFeeAssetId,
+            maxPayment: maxPayment,
+          };
+          const transactionPayment =
+            userFeeAssetId.toString() === networkFeeAssetId.toString()
+              ? null
+              : {
+                  tip: 0,
+                  feeExchange: {
+                    FeeExchangeV1: feeExchange,
+                  },
+                };
+
+          const payload = {runtimeVersion, era, blockHash, genesisHash, nonce, transactionPayment};
+          (extrinsic as Extrinsic).signFake(sender, payload as any);
+          return combineLatest([api.rpc.payment.queryInfo(extrinsic.toHex()), of(networkFeeAssetId)]);
         }
       ),
-      switchMap(paymentInfo => {
+      switchMap(([paymentInfo, networkFeeAssetId]) => {
         const feeInBaseCurrency = paymentInfo.partialFee;
-        if (feeAssetId.toString() === DEFAULT_FEE_ASSET) {
+        if (userFeeAssetId.toString() === networkFeeAssetId.toString()) {
           return of(feeInBaseCurrency);
         } else {
-          return (api.rpc as any).cennzx.buyPrice(DEFAULT_FEE_ASSET, feeInBaseCurrency, feeAssetId);
+          return (api.rpc as any).cennzx.buyPrice(networkFeeAssetId, feeInBaseCurrency, userFeeAssetId);
         }
       }),
       map((price: BN) => price),
