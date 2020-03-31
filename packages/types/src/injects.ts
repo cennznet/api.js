432ab09478c71a45f9d5870cfd7d1c3fdf0d03be
karishma <bothara.karishma@gmail.com>
2020-04-01 11:16:32 +1300

    removed unused import

--- packages/types/src/injects.ts
+++ packages/types/src/injects.ts
@@ -11,7 +11,6 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-import {StorageHasherV11} from '@cennznet/types/extrinsic/types';
 import Doughnut from './Doughnut';
 import * as extrinsicTypes from './extrinsic';
 import metadata from './metaDataTypes';
