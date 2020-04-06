71012d47c1d7c461aeeb26b8dedc939361f9d25c
karishma <bothara.karishma@gmail.com>
2020-04-06 12:02:52 +1200

    wip

--- packages/types/src/extrinsic/v2/Extrinsic.ts
+++ packages/types/src/extrinsic/v2/Extrinsic.ts
@@ -121,7 +121,7 @@ export default class ExtrinsicV2 extends Struct implements IExtrinsicImpl {
   /**
    * @describe Adds a fake signature to the extrinsic
    */
-  signFake(signer: Address | Uint8Array | string, options: ExtrinsicV2SignatureOptions): ExtrinsicV2 {
+  signFake(signer: Address | Uint8Array | string, options: SignatureOptions): ExtrinsicV2 {
     this.signature.signFake(this.method, signer, options);
 
     return this;
@@ -129,7 +129,7 @@ export default class ExtrinsicV2 extends Struct implements IExtrinsicImpl {
   /**
    * @description Sign the extrinsic with a specific keypair
    */
-  sign(account: IKeyringPair, options: ExtrinsicV2SignatureOptions): ExtrinsicV2 {
+  sign(account: IKeyringPair, options: SignatureOptions): ExtrinsicV2 {
     this.signature.sign(this.method, account, options);
     // console.log('Extra option:', JSON.stringify(options));
 
