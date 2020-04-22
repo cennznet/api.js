ff259837a72fd7054346506e572259deb87ff4e9
Eric <e.lim0322@gmail.com>
2020-04-22 16:04:48 +1200

    Add Message and MessageId types for Sylo

--- packages/types/src/runtime/sylo/index.ts
+++ packages/types/src/runtime/sylo/index.ts
@@ -127,3 +127,7 @@ export class Response extends Enum.with({DeviceIdResponse, PreKeyBundlesResponse
 export class VaultKey extends ClassOf(registry, 'Bytes') {}
 
 export class VaultValue extends ClassOf(registry, 'Bytes') {}
+
+export class MessageId extends ClassOf(registry, 'u32') {}
+
+export class Message extends ClassOf(registry, 'Bytes') {}
