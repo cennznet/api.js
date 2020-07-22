f7e08021aefe050660f56afe922b60009b6abbb4
karishma <bothara.karishma@gmail.com>
2020-07-23 10:21:42 +1200

    used new doughnut-rs package and comment review changes

--- packages/api/test/e2e/doughnut.e2e.ts
+++ packages/api/test/e2e/doughnut.e2e.ts
@@ -1,4 +1,4 @@
-// Copyright 2019 Centrality Investments Limited
+// Copyright 2020 Centrality Investments Limited
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -12,34 +12,32 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-import testingPairs from '@polkadot/keyring/testingPairs';
-import { hexToU8a, assert } from '@polkadot/util'
-import { CENNZnut as encodeCennznut} from '@cennznet/cennznut-wasm';
-import { Doughnut as DoughnutMaker } from '@plugnet/doughnut-wasm';
-// import { Doughnut as DoughnutMaker } from '/Users/karishma/WebstormProjects/doughnut-rs/js/libNode/Doughnut';
-import { Api } from '../../src/Api';
-import { Keypair } from '@plugnet/util-crypto/types';
+import {hexToU8a, assert} from '@polkadot/util';
+import {CENNZnut as encodeCennznut} from '@cennznet/cennznut-wasm';
+import {Doughnut as DoughnutMaker} from '@plugnet/doughnut-wasm';
+import {Api} from '../../src/Api';
+import {Keypair} from '@plugnet/util-crypto/types';
 import {Keyring} from '@polkadot/api';
-import { KeyringPair } from '@plugnet/keyring/types';
+import {KeyringPair} from '@plugnet/keyring/types';
 import initApiPromise from '../../../../jest/initApiPromise';
 import {cryptoWaitReady} from '@plugnet/util-crypto';
 import Doughnut from '@cennznet/types/Doughnut';
 
-// Helper for creating CENNZnuts
+/// Helper for creating CENNZnuts which takes module/section name and method name to be same for doughnut permissioning
 function makeCennznut(module: string, method: string): Uint8Array {
     const moduleVec = [
         [
-            module,  {
-                        "name":module,
-                        "methods":[
-                            [
-                                method,  {
-                                    "name":method,
-                                    "constraints":null
-                                }
-                            ],
-                        ]
-                    }
+            module, {
+            'name': module,
+            'methods': [
+                [
+                    method, {
+                    'name': method,
+                    'constraints': null,
+                },
+                ],
+            ],
+        },
         ],
     ];
     const contractVec = [];
@@ -47,37 +45,35 @@ function makeCennznut(module: string, method: string): Uint8Array {
     return cennznut.encode();
 }
 
-/// Helper for creating v0 Doughnuts
+/// Helper for creating v0 Doughnuts where user can pass expiry time for doughnut to be valid and 'not before' which would mean doughnut is not applied before this block
 function makeDoughnut(
-   issuer: Keypair,
+    issuer: Keypair,
     holder: KeyringPair,
-   permissions: Record<string, Uint8Array>,
+    permissions: Record<string, Uint8Array>,
 ) {
     const d = new DoughnutMaker(
-            issuer.publicKey,
-            holder.publicKey,
-            2595374352,// expiry
-            1, //notBefore
+        issuer.publicKey,
+        holder.publicKey,
+        (new Date().getTime()) * 2, // expiry set to twice the current timestamp
+        1, //notBefore
     );
     for (const key in permissions) {
-        d.addDomain(key,permissions[key]);
+        d.addDomain(key, permissions[key]);
     }
-   d.signSr25519(new Uint8Array(issuer.secretKey));
+    d.signSr25519(new Uint8Array(issuer.secretKey));
     return d.encode();
 }
 
 
-
-
 const Dave = {
     address: '5DAAnrj7VHTznn2AWBemMuyBwZWs6FNFjdyVXUeYum3PTXFy',
-    uri: '//Dave'
-}
+    uri: '//Dave',
+};
 
 describe('Doughnut for CennznetExtrinsic', () => {
     let aliceKeyPair = {
         secretKey: hexToU8a('0x98319d4ff8a9508c4bb0cf0b5a78d760a0b2082c02775e6e82370816fedfff48925a225d97aa00682d6a59b95b18780c10d7032336e88f3442b42361f4a66011'),
-        publicKey: hexToU8a('0xd43593c715fdd31c61141abd04a99fd6822c8558854ccde39a5684e7a56da27d')
+        publicKey: hexToU8a('0xd43593c715fdd31c61141abd04a99fd6822c8558854ccde39a5684e7a56da27d'),
     };
 
     let api: Api;
@@ -90,7 +86,7 @@ describe('Doughnut for CennznetExtrinsic', () => {
     beforeAll(async () => {
         api = await initApiPromise();
         await cryptoWaitReady();
-        const keyring = new Keyring({ type: 'sr25519' });
+        const keyring = new Keyring({type: 'sr25519'});
         alice = keyring.addFromUri('//Alice');
         bob = keyring.addFromUri('//Bob');
         charlie = keyring.addFromUri('//Charlie');
@@ -106,7 +102,7 @@ describe('Doughnut for CennznetExtrinsic', () => {
         let doughnut = makeDoughnut(
             aliceKeyPair,
             holderKeyPair,
-            { "cennznet": makeCennznut("generic-asset", "transfer") }
+            {'cennznet': makeCennznut('generic-asset', 'transfer')},
         );
         const CENNZ = 16000;
         const reciever = Dave.address;
@@ -119,7 +115,7 @@ describe('Doughnut for CennznetExtrinsic', () => {
             if (status.isFinalized) {
                 const blockHash = status.value.toHex();
                 console.log('Completed at block hash', blockHash);
-                const allEvents = await api.query.system.events.at(blockHash)
+                const allEvents = await api.query.system.events.at(blockHash);
                 const transfer = allEvents.find(
                     event => (
                         event.event.data.method === 'Transferred' &&
@@ -128,7 +124,7 @@ describe('Doughnut for CennznetExtrinsic', () => {
                         event.event.data[0].toString() === CENNZ.toString() && // CENNZ
                         event.event.data[2].toString() === reciever && // Dave is reciever
                         event.event.data[3].toString() === amount.toString()
-                    )
+                    ),
                 );
                 allEvents.forEach(({phase, event: {data, method, section}}) => {
                     console.log('\t', phase.toString(), `: ${section}.${method}`, data.toString());
@@ -136,7 +132,7 @@ describe('Doughnut for CennznetExtrinsic', () => {
                 if (transfer != undefined) {
                     done();
                 } else {
-                    assert(true, "false");
+                    assert(true, 'false');
                 }
             }
         });
@@ -146,21 +142,21 @@ describe('Doughnut for CennznetExtrinsic', () => {
         let doughnut = await makeDoughnut(
             aliceKeyPair,
             bob,
-            { "cennznet": makeCennznut("generic-asset", "transfer") }
+            {'cennznet': makeCennznut('generic-asset', 'transfer')},
         );
         const doughnutB = new Doughnut(api.registry, doughnut);
         const opt = {doughnut: doughnutB};
         const tx = api.tx.genericAsset.transfer(16001, charlie.address, 10000);
 
-        await expect(tx.signAndSend(charlie, opt as any,() => { })).
-        rejects.toThrow("submitAndWatchExtrinsic(extrinsic: Extrinsic): ExtrinsicStatus:: 1010: Invalid Transaction: {\"Custom\":180}");
+        await expect(tx.signAndSend(charlie, opt as any, () => {
+        })).rejects.toThrow('submitAndWatchExtrinsic(extrinsic: Extrinsic): ExtrinsicStatus:: 1010: Invalid Transaction: {"Custom":180}');
     });
 
     it('fails when cennznut does not authorize the extrinsic method', async (done) => {
         let doughnut = await makeDoughnut(
             aliceKeyPair,
             bob,
-           { "cennznet": makeCennznut("generic-asset", "mint") }
+            {'cennznet': makeCennznut('generic-asset', 'mint')},
         );
         const doughnutB = new Doughnut(api.registry, doughnut);
         const opt = {doughnut: doughnutB};
@@ -175,7 +171,7 @@ describe('Doughnut for CennznetExtrinsic', () => {
                 if (failed != undefined) {
                     done();
                 } else {
-                    assert(false, "expected extrinsic to fail");
+                    assert(false, 'expected extrinsic to fail');
                 }
             }
         });
@@ -186,7 +182,7 @@ describe('Doughnut for CennznetExtrinsic', () => {
         let doughnut = await makeDoughnut(
             aliceKeyPair,
             bob,
-           { "cennznet": makeCennznut("balance", "transfer") }
+            {'cennznet': makeCennznut('balance', 'transfer')},
         );
 
         const tx = api.tx.genericAsset.transfer(16001, charlie.address, 10000);
@@ -197,12 +193,12 @@ describe('Doughnut for CennznetExtrinsic', () => {
             if (status.isFinalized) {
                 const blockHash = status.value.toHex();
                 console.log('Completed at block hash', blockHash);
-                const allEvents = await api.query.system.events.at(blockHash)
+                const allEvents = await api.query.system.events.at(blockHash);
                 const failed = allEvents.find(event => event.event.data.method === 'ExtrinsicFailed');
                 if (failed != undefined) {
                     done();
                 } else {
-                    assert(false, "expected extrinsic to fail");
+                    assert(false, 'expected extrinsic to fail');
                 }
             }
         });
