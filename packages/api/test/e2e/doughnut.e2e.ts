d61d2ffb9b33c7f34bde67caa487ba0b1a7a8ec5
karishma <bothara.karishma@gmail.com>
2020-07-31 10:44:26 +1200

    as per review comments

--- packages/api/test/e2e/doughnut.e2e.ts
+++ packages/api/test/e2e/doughnut.e2e.ts
@@ -16,7 +16,7 @@ import { hexToU8a } from '@polkadot/util';
 import { CENNZnut as encodeCennznut } from '@cennznet/cennznut-wasm';
 import { Doughnut as DoughnutMaker } from '@plugnet/doughnut-wasm';
 import { Api } from '../../src/Api';
-import { Keypair } from '@plugnet/util-crypto/types';
+import { Keypair } from '@polkadot/util-crypto/types';
 import { Keyring } from '@polkadot/api';
 import { KeyringPair } from '@plugnet/keyring/types';
 import initApiPromise from '../../../../jest/initApiPromise';
@@ -30,6 +30,12 @@ describe('Doughnut for CennznetExtrinsic', () => {
     secretKey: hexToU8a('0x98319d4ff8a9508c4bb0cf0b5a78d760a0b2082c02775e6e82370816fedfff48925a225d97aa00682d6a59b95b18780c10d7032336e88f3442b42361f4a66011'),
     publicKey: hexToU8a('0xd43593c715fdd31c61141abd04a99fd6822c8558854ccde39a5684e7a56da27d'),
     address: '5GrwvaEF5zXb26Fz9rcQpDWS57CtERHpNehXCPcNoHGKutQY'
+  };
+
+  let bobKeyPair = {
+    secretKey: hexToU8a('0x081ff694633e255136bdb456c20a5fc8fed21f8b964c11bb17ff534ce80ebd5941ae88f85d0c1bfc37be41c904e1dfc01de8c8067b0d6d5df25dd1ac0894a325'),
+    publicKey: hexToU8a('0x8eaf04151687736326c9fea17e25fc5287613693c912909cb226aa4794f26a48'),
+    address: '5FHneW46xGXgs5mUiveU4sbTyGBzmstUspZC92UhjJM694ty'
   };
 
   let api: Api;
@@ -72,7 +78,7 @@ describe('Doughnut for CennznetExtrinsic', () => {
     });
   });
 
-  it('fails when charlie uses bob\'s doughnut', async () => {
+  it('Fails when charlie uses bob\'s doughnut', async () => {
     let doughnut = makeDoughnut(
       api.registry,
       {
@@ -89,7 +95,7 @@ describe('Doughnut for CennznetExtrinsic', () => {
       .toThrow('submitAndWatchExtrinsic(extrinsic: Extrinsic): ExtrinsicStatus:: 1010: Invalid Transaction: {"Custom":180}')
   });
 
-  it('fails when cennznut does not authorize the extrinsic method', async done => {
+  it('Fails when cennznut does not authorize the extrinsic method', async done => {
     let doughnut = makeDoughnut(
       api.registry,
       {
@@ -110,7 +116,7 @@ describe('Doughnut for CennznetExtrinsic', () => {
     });
   });
 
-  it('fails when cennznut does not authorize the extrinsic module', async done => {
+  it('Fails when cennznut does not authorize the extrinsic module', async done => {
     let badDoughnut = makeDoughnut(
       api.registry,
       {
@@ -166,6 +172,43 @@ describe('Doughnut for CennznetExtrinsic', () => {
      })).rejects.toThrow('submitAndWatchExtrinsic(extrinsic: Extrinsic): ExtrinsicStatus:: 1010: Invalid Transaction: {"Custom":172}');
      done();
   });
+
+  it('Fails when doughnuts notBefore is set for future block', async done => {
+     let doughnut = await makeDoughnut(
+        api.registry,
+         {
+             issuer: aliceKeyPair,
+             holder: bob,
+             permissions: {'cennznet': makeCennznut('balance', 'transfer')},
+             notBefore: (new Date().getTime() / 1000) + 100,
+         },
+     );
+
+     const tx = api.tx.genericAsset.transfer(CENNZ, charlie.address, 10_000);
+     await expect(tx.signAndSend(bob, {doughnut: doughnut}, () => {
+         // https://github.com/cennznet/cennznet/wiki/Transaction-Error-Codes#doughnut-usage
+     })).rejects.toThrow('submitAndWatchExtrinsic(extrinsic: Extrinsic): ExtrinsicStatus:: 1010: Invalid Transaction: {"Custom":182}');
+     done();
+  });
+
+  it('Fails when doughnut is not signed by issuer', async done => {
+     let doughnut = await makeDoughnut(
+         api.registry,
+         {
+             issuer: aliceKeyPair,
+             holder: bob,
+             permissions: {'cennznet': makeCennznut('balance', 'transfer')},
+             notBefore: (new Date().getTime() / 1000) + 100,
+             signer: bobKeyPair
+         },
+     );
+
+     const tx = api.tx.genericAsset.transfer(CENNZ, charlie.address, 10_000);
+     await expect(tx.signAndSend(bob, {doughnut: doughnut}, () => {
+         // https://github.com/cennznet/cennznet/wiki/Transaction-Error-Codes#doughnut-usage
+     })).rejects.toThrow('submitAndWatchExtrinsic(extrinsic: Extrinsic): ExtrinsicStatus:: 1010: Invalid Transaction: {"Custom":170}');
+     done();
+  });
 });
 
 interface DoughnutArgs {
@@ -173,7 +216,9 @@ interface DoughnutArgs {
   holder: KeyringPair,
   permissions: Record<string, Uint8Array>,
   expiry?: number,
-  isSigned?: boolean
+  isSigned?: boolean,
+  notBefore?: number,
+  signer?: Keypair
 }
 
 /// Helper for creating CENNZnuts which takes module/section name and method name to be same for doughnut permissioning
@@ -200,44 +245,23 @@ function makeCennznut(module: string, method: string): Uint8Array {
 
 // Helper for creating v0 Doughnuts where user can pass expiry time for doughnut to be valid and
 // 'not before' which would mean doughnut is not applied before this block
-function makeDoughnut(registry, { issuer, holder, permissions, expiry = (new Date().getTime() / 1000) + 600, isSigned = true }: DoughnutArgs): Doughnut {
+function makeDoughnut(registry,
+                      { issuer, holder, permissions, expiry = (new Date().getTime() / 1000) + 600, isSigned = true, notBefore = 1, signer }: DoughnutArgs): Doughnut {
   const d = new DoughnutMaker(
     issuer.publicKey,
     holder.publicKey,
     expiry, // default expire in 10 minutes
-    1, // 'not before'
+    notBefore, // default 'not before' is set to 1
   );
   for (const key in permissions) {
     d.addDomain(key, permissions[key]);
   }
-  if (isSigned) {
+  if (signer) {
+      d.signSr25519(new Uint8Array(signer.secretKey));
+  }
+  else if (isSigned) {
       d.signSr25519(new Uint8Array(issuer.secretKey));
   }
   let encoded = d.encode();
   return new Doughnut(registry, encoded)
-}
-
-function checkEventMatches({ allEvents, method, section, sender, receiver, amount, asset }) {
-  if (method === 'Transferred') {
-    return allEvents.find(
-      event => (
-        event.event.data.method === method &&
-        event.event.data.section === section &&
-        event.event.data[1].toString() === sender &&
-        event.event.data[0].toString() === asset &&
-        event.event.data[2].toString() === receiver &&
-        event.event.data[3].toString() === amount
-      ),
-    );
-  } else if (method === 'Minted') {
-    return allEvents.find(
-      event => (
-        event.event.data.method === method &&
-        event.event.data.section === section &&
-        event.event.data[1].toString() === receiver &&
-        event.event.data[0].toString() === asset &&
-        event.event.data[2].toString() === amount
-      ),
-    );
-  }
 }
