6e998b1c6c06545a36a5f3588cd3c8c7a7d36fc3
karishma <bothara.karishma@gmail.com>
2020-08-13 11:12:27 +1200

    fix

--- packages/api/src/derives/index.ts
+++ packages/api/src/derives/index.ts
@@ -20,11 +20,12 @@ import {AnyFunction} from '@cennznet/types/types';
 import {ApiInterfaceRx, MethodResult} from '@polkadot/api/types';
 import {Observable} from 'rxjs';
 import * as fees from './fees';
+import * as session from './session';
 import * as staking from './staking';
 
 export type DeriveFunc = (api: ApiInterfaceRx) => (...args: any[]) => Observable<any>;
 
-export const derive = {attestation, cennzxSpot, fees, staking, genericAsset};
+export const derive = {attestation, cennzxSpot, fees, staking, session, genericAsset};
 
 export type DecoratedCennznetDerive<
   ApiType extends ApiTypes,
