52cc658c28d24a81e1e62f52d0b0e5bd1a911462
AlexSedNZ <alex.sedighi@centrality.ai>
2020-08-18 14:59:04 +1200

    Fix cennznet RPCs

--- packages/api/src/rpc/index.ts
+++ packages/api/src/rpc/index.ts
@@ -2,10 +2,8 @@ import cennzxRpc from './cennzx';
 import genericAssetRpc from './genericAsset';
 
 const interfaces = {
-  rpc: {
-    cennzx: cennzxRpc,
-    genericAsset: genericAssetRpc,
-  },
+  cennzx: cennzxRpc,
+  genericAsset: genericAssetRpc,
 };
 
 export default interfaces;
