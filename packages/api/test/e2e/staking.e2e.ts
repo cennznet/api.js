e6762f3f66feba618c52038e8764bc742cf18da6
karishma <bothara.karishma@gmail.com>
2020-09-23 09:23:25 +1200

    Fixed a staking test that is failing on develop branch

--- packages/api/test/e2e/staking.e2e.ts
+++ packages/api/test/e2e/staking.e2e.ts
@@ -281,27 +281,20 @@ describe('Staking Governance (Sudo Required)', () => {
   });
 
   test('Force unstake', async done => {
-    const bob_stash = keyring.addFromUri('//Bob//stash');
+    const bobStash = keyring.addFromUri('//Bob//stash');
     // bond bob's stash account.
     await api.tx.staking.bond(bob.address, 10_000, 'controller').signAndSend(
-      bob_stash,
+      bobStash,
       async ({ status }) => {
         if (status.isInBlock) {
-          const controller = (await api.query.staking.bonded(bob_stash.address)) as Option<AccountId>;
+          const controller = (await api.query.staking.bonded(bobStash.address)) as Option<AccountId>;
           expect(controller.unwrapOr(null)).toBeDefined();
-          done();
+          const unstake = api.tx.staking.forceUnstake(bobStash.address);
+          await api.tx.sudo.sudo(unstake).signAndSend(alice);
+          await api.query.staking.bonded(bobStash.address, (controller: Option<AccountId>) =>
+              (controller.unwrapOr(null) === null) ? done() : null);
         }
       });
-
-    const unstake = api.tx.staking.forceUnstake(bob_stash.address);
-    await api.tx.sudo.sudo(unstake).signAndSend(alice, async ({ status }) => {
-      if (status.isInBlock) {
-        // bob stash is removed / unbonded
-        const controller = (await api.query.staking.bonded(bob_stash.address)) as Option<AccountId>;
-        expect(controller.unwrapOr(null)).toBeNull();
-        done();
-      }
-    });
   });
 
 });
