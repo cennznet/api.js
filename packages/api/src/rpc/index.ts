4c539f051f5a3aebc080a0099a4b0959f02a3c6a
AlexSedNZ <alex.sedighi@centrality.ai>
2020-10-20 10:00:25 +1300

    Remove Payment queryInfo from custom rpcs.

--- packages/api/src/rpc/index.ts
+++ packages/api/src/rpc/index.ts
@@ -1,11 +1,9 @@
 import cennzxRpc from './cennzx';
 import genericAssetRpc from './genericAsset';
-import paymentRpc from './payment';
 
 const interfaces = {
   cennzx: cennzxRpc,
   genericAsset: genericAssetRpc,
-  payment: paymentRpc,
 };
 
 export default interfaces;
