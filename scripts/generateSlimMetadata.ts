5d2c3b8c9b1b4286d57bcee305f723215d43a2db
KarishmaBothara <bothara.karishma@gmail.com>
2020-10-20 10:04:50 +1300

    reformatted with prettier

--- scripts/generateSlimMetadata.ts
+++ scripts/generateSlimMetadata.ts
@@ -1,5 +1,5 @@
 // Running this file with ~ yarn generate-slim-metadata 'wss://cennznet.unfrastructure.io/public/ws' will create slim metadata
-import {Api} from '../packages/api/src/Api';
+import { Api } from '../packages/api/src/Api';
 import MetadataVersioned from '@polkadot/metadata/Metadata/MetadataVersioned';
 
 // Metadata is slimmed as follows:
@@ -12,75 +12,74 @@ import MetadataVersioned from '@polkadot/metadata/Metadata/MetadataVersioned';
 // END POINT can be passed as command line argument - yarn generate-slim-metadata 'ws://localhost:9944', by default Azalea would be used
 
 async function generateSlimMeta() {
-    const provider: any = process.argv[2] ? process.argv[2]: 'wss://cennznet.unfrastructure.io/public/ws';
-    const api = await Api.create({provider});
-    const metadata = api.runtimeMetadata;
-    const KEEP = [
-        "System",
-        "Timestamp",
-        "TransactionPayment",
-        "GenericAsset",
-        "SyloGroups",
-        "SyloE2EE",
-        "SyloDevice",
-        "SyloInbox",
-        "SyloResponse",
-        "SyloVault",
-        "CennzxSpot",
-    ];
-    let magicNumber = metadata.magicNumber;
-    let modules = api.runtimeMetadata.asLatest.modules;
-    let newModules = [];
-    for (const m of modules) {
-        if (KEEP.indexOf(m.name.toJSON()) >= 0) {
-            const module = m.toJSON();
-            removeKeys(module, "documentation");
-            // Creating ModuleMetadataLatest
-            let modifiedModule = api.registry.createType( 'ModuleMetadataLatest', module);
-            newModules.push(modifiedModule);
-        } else {
-            // Push an empty module
-            newModules.push({"name": m.name, "calls": m.calls, "events": m.events});
-        }
+  const provider: any = process.argv[2] ? process.argv[2] : 'wss://cennznet.unfrastructure.io/public/ws';
+  const api = await Api.create({ provider });
+  const metadata = api.runtimeMetadata;
+  const KEEP = [
+    'System',
+    'Timestamp',
+    'TransactionPayment',
+    'GenericAsset',
+    'SyloGroups',
+    'SyloE2EE',
+    'SyloDevice',
+    'SyloInbox',
+    'SyloResponse',
+    'SyloVault',
+    'CennzxSpot',
+  ];
+  let magicNumber = metadata.magicNumber;
+  let modules = api.runtimeMetadata.asLatest.modules;
+  let newModules = [];
+  for (const m of modules) {
+    if (KEEP.indexOf(m.name.toJSON()) >= 0) {
+      const module = m.toJSON();
+      removeKeys(module, 'documentation');
+      // Creating ModuleMetadataLatest
+      let modifiedModule = api.registry.createType('ModuleMetadataLatest', module);
+      newModules.push(modifiedModule);
+    } else {
+      // Push an empty module
+      newModules.push({ name: m.name, calls: m.calls, events: m.events });
     }
-    let extrinsic = metadata.asLatest.extrinsic;
-    let filteredModule = api.registry.createType( 'Vec<ModuleMetadataLatest>', newModules);
-    let filteredMetaLatest = api.registry.createType( 'MetadataLatest', {modules: filteredModule, extrinsic});
-    const filteredMetadataAll = api.registry.createType('MetadataAll', filteredMetaLatest, 12);
-    const mVersionedNew = new MetadataVersioned(api.registry, {
-        magicNumber: magicNumber,
-        metadata: filteredMetadataAll
-    });
-    console.log('metadata hex:', mVersionedNew.toHex());
+  }
+  let extrinsic = metadata.asLatest.extrinsic;
+  let filteredModule = api.registry.createType('Vec<ModuleMetadataLatest>', newModules);
+  let filteredMetaLatest = api.registry.createType('MetadataLatest', { modules: filteredModule, extrinsic });
+  const filteredMetadataAll = api.registry.createType('MetadataAll', filteredMetaLatest, 12);
+  const mVersionedNew = new MetadataVersioned(api.registry, {
+    magicNumber: magicNumber,
+    metadata: filteredMetadataAll,
+  });
+  console.log('metadata hex:', mVersionedNew.toHex());
 
-    process.exit();
+  process.exit();
 }
 
-function removeKeys(obj, keys){
-    var index;
-    for (var prop in obj) {
-        // important check that this is objects own property
-        // not from prototype prop inherited
-        if(obj.hasOwnProperty(prop)){
-            switch(typeof(obj[prop])){
-                case 'string':
-                    index = keys.indexOf(prop);
-                    if(index > -1){
-                        delete obj[prop];
-                    }
-                    break;
-                case 'object':
-                    index = keys.indexOf(prop);
-                    if(index > -1){
-                        delete obj[prop];
-                    }else{
-                        removeKeys(obj[prop], keys);
-                    }
-                    break;
-            }
-        }
+function removeKeys(obj, keys) {
+  let index;
+  for (const prop in obj) {
+    // important check that this is objects own property
+    // not from prototype prop inherited
+    if (obj.hasOwnProperty(prop)) {
+      switch (typeof obj[prop]) {
+        case 'string':
+          index = keys.indexOf(prop);
+          if (index > -1) {
+            delete obj[prop];
+          }
+          break;
+        case 'object':
+          index = keys.indexOf(prop);
+          if (index > -1) {
+            delete obj[prop];
+          } else {
+            removeKeys(obj[prop], keys);
+          }
+          break;
+      }
     }
+  }
 }
-
 
 generateSlimMeta();
