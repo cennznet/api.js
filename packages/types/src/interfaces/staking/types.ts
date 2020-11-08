6b41fb0eb26edc84386eebef35c41cfde9576e8b
KarishmaBothara <bothara.karishma@gmail.com>
2020-11-09 11:15:41 +1300

    clean up

--- packages/types/src/interfaces/staking/types.ts
+++ packages/types/src/interfaces/staking/types.ts
@@ -1,10 +1,8 @@
 // Auto-generated via `yarn polkadot-types-from-defs`, do not edit
 /* eslint-disable */
 
-import { Enum, Vec } from '@polkadot/types/codec';
+import { Enum } from '@polkadot/types/codec';
 import { AccountId, Balance } from '@polkadot/types/interfaces/runtime';
-// import VecAny from "@polkadot/types/codec/VecAny";
-import { Codec } from '@polkadot/types/types';
 
 /** @name RewardBalance */
 export interface RewardBalance extends Balance {}
