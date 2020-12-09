54c168a61c7b4af3a663744e30a4b6ea13e6eb01
KarishmaBothara <bothara.karishma@gmail.com>
2020-12-09 18:01:24 +1300

    as per discussion

--- packages/api/test/e2e/send.e2e.ts
+++ packages/api/test/e2e/send.e2e.ts
@@ -1,17 +1,23 @@
-import { WsProvider } from "@polkadot/rpc-provider";
+import WebSocket from '@polkadot/x-ws';
+import { RpcCoder } from "@polkadot/rpc-provider/coder";
 
 describe('send', (): void => {
-    let ws: WsProvider;
 
-    beforeEach((): void => {
+    it('Send request using websocket', async (done)  => {
         const providerUrl = 'wss://cennznet.unfrastructure.io/public/ws';
-        ws = new WsProvider(providerUrl);
-    });
-
-    it('Send request using ws provider', async ()  => {
-        const hash = await ws.send('chain_getBlockHash', []);
-        expect(hash).toBeDefined();
-        const chain = await ws.send('system_chain', []);
-        expect(chain).toBe('CENNZnet Azalea');
+        const ws = new WebSocket(providerUrl);
+        const coder = new RpcCoder();
+        ws.onopen = function() {
+            const requestObject = coder.encodeJson('system_chain', []);
+            ws.send(requestObject);
+            ws.onmessage = function (event) {
+                expect(event.data).toEqual('{"jsonrpc":"2.0","result":"CENNZnet Azalea","id":1}');
+                const data = JSON.parse(event.data);
+                expect(data.result).toEqual('CENNZnet Azalea');
+                expect(data.id).toBeGreaterThan(0);
+                expect(data.jsonrpc).toEqual("2.0");
+                done();
+            }
+        };
     });
 });
