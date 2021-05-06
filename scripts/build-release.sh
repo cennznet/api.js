d4ab77c29dc74f9875ebcbab18580f2fe0606ae3
holygits <beauchjord@gmail.com>
2021-05-07 11:53:05 +1200

    fix command

--- scripts/build-release.sh
+++ scripts/build-release.sh
@@ -3,13 +3,15 @@ set -e
 BUILD_DIR="build"
 rm -rf $BUILD_DIR
 printf '\n\nChecking TS definitions..\n\n'
-# this is just for type checking we don't publish the output files.
-# babel will produce the real *.d.ts file within each package/build dir.
+# generate typescript definitions
 tsc --outDir $BUILD_DIR
 
 for d in $(pwd)/packages/* ; do
-    printf "\n\nBuilding $d...\n\n"
+    PACKAGE=$(basename $d)
+    printf "\n\nBuilding @cennznet/$PACKAGE...\n\n"
     npx babel -f babel.config.js --ignore='**/*.d.ts' --extensions='.ts' --out-file-extension='.js' --copy-files -d "$d/build" "$d/src"
+    # copy in *d.ts definitions
+    rsync -a "$BUILD_DIR/packages/$PACKAGE/src/" "$d/build/"ls
     # copy in essential files for publishing to npm
     cp LICENSE $d/build/LICENSE
     ls $d
