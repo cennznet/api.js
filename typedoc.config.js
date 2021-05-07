3df6c3a1d7e0ecdf82930000ea2d3ffe896f8d9d
holygits <beauchjord@gmail.com>
2021-05-07 21:22:28 +1200

    fix typedoc

--- typedoc.config.js
+++ typedoc.config.js
@@ -1,28 +1,20 @@
 module.exports = {
   "name": "@cennznet/api",
   "out": "./api_docs",
-  "mode": "modules",
+  "entryPoints": ["packages/api/src"],
   "exclude": [
-    "**/node_modules/!(@plugnet|@cennznet)/**",
-    "**/node_modules/@cennznet/*/node_modules/**",
-    "**/node_modules/@plugnet/**/json.d.ts",
-    "**/node_modules/@plugnet/dev/**/*",
+    "**/node_modules",
     "**/packages/**",
     "scripts/*",
+    "*.e2e.ts",
     "*.spec.*",
     "**/test/**",
     "**/build/**"
   ],
   "excludeExternals": false,
   "externalPattern": "**/node_modules/**",
-  "external-modulemap": [
-    ["@cennznet/api", "(.*packages\/api\/.*|node_modules\/@cennznet\/api\/.*)", "packages/api/README.md"],
-    ["@cennznet/types", "(.*packages\/types\/.*|node_modules\/@cennznet\/types\/.*)", "packages/types/README.md"],
-  ],
   "excludePrivate": true,
   "excludeProtected": true,
-  "includeDeclarations": true,
-  "ignoreCompilerErrors": true,
   "theme": "markdown",
   "tsconfig": "tsconfig.json",
   "readme": "none"
