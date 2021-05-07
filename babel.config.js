bae59c593d57c586bbf8331574f8f637146c3914
holygits <beauchjord@gmail.com>
2021-05-07 13:09:16 +1200

    old babel conf

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,20 @@
 module.exports = {
-  extends: '@polkadot/dev/config/babel-config-cjs.cjs',
-  "plugins": [
-    ["@babel/plugin-proposal-private-methods", { "loose": true }]
+  presets: [
+    ['@babel/preset-env', {
+        modules: 'commonjs',
+        targets: {
+          browsers: '>0.25% and last 2 versions and not ie 11 and not OperaMini all',
+          node: '10'
+        }
+    }],
+    '@babel/preset-typescript'
+  ],
+  plugins: [
+    ['@babel/plugin-proposal-class-properties', { loose: true }],
+    '@babel/plugin-proposal-numeric-separator',
+    '@babel/plugin-proposal-object-rest-spread',
+    '@babel/plugin-proposal-optional-chaining',
+    '@babel/plugin-transform-runtime',
+    ['@babel/plugin-proposal-private-methods', { loose: true }]
   ]
 };
