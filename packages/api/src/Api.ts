5d70a2a31e8929c3cdef947f5098a2058d433e21
KarishmaBothara <bothara.karishma@gmail.com>
2021-05-10 13:26:30 +1200

    debugging rpc object before creating API instance

--- packages/api/src/Api.ts
+++ packages/api/src/Api.ts
@@ -71,6 +71,7 @@ export class Api extends ApiPromise {
         rpc[section] = value.rpc;
       }
     });
+    console.log('rpc:::', rpc);
     options.metadata = Object.assign(staticMetadata, options.metadata);
     options.types = { ...options.types, ...Types };
     options.derives = mergeDeriveOptions(derives, options.derives);
