8db004275aa1978fd3ffd562ab4943cbefc919c4
KarishmaBothara <bothara.karishma@gmail.com>
2021-07-05 09:11:57 +1200

    updated file

--- packages/types/src/runtimeModuleTypes.ts
+++ packages/types/src/runtimeModuleTypes.ts
@@ -1,18 +1,18 @@
 export default {
-    Direction: {
-      _enum: ["Up", "Left", "Down", "Right"]
+    "Direction": {
+      "_enum": ["Up", "Left", "Down", "Right"]
     },
-    Food: {
-      x: "i8",
-      y: "i8"
+  "Food": {
+    "x": "i8",
+    "y": "i8"
     },
-    Snake: {
-      body: "Vec<(i8,i8)>",
-      dir: "Direction",
-      direction_changed: "bool"
+  "Snake": {
+    "body": "Vec<(i8,i8)>",
+    "dir": "Direction",
+    "direction_changed": "bool"
     },
-    WindowSize: {
-      window_width: "i8",
-      window_height: "i8"
+  "WindowSize": {
+    "window_width": "i8",
+    "window_height": "i8"
     }
 }
