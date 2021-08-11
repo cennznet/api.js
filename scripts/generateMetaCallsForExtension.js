4e4a47250c284de91496729e50a5b4895ec90eed
KarishmaBothara <bothara.karishma@gmail.com>
2021-08-12 11:30:25 +1200

    fixes https://github.com/cennznet/api.js/runs/3296517995?check_suite_focus=true extension metadata job

--- scripts/generateMetaCallsForExtension.js
+++ scripts/generateMetaCallsForExtension.js
@@ -2,7 +2,7 @@
 // Copyright 2017-2019 @polkadot/types authors & contributors
 // This software may be modified and distributed under the terms
 // of the Apache-2.0 license. See the LICENSE file for details.
-
+process.env.JEST_WORKER_ID = '123';
 require('@babel/register')({
   extensions: ['.js', '.ts'],
   plugins: [
@@ -10,7 +10,6 @@ require('@babel/register')({
       alias: {
         '^@cennznet/api(.*)': './packages/api/src\\1',
         '^@cennznet/types(.*)': './packages/types/src\\1',
-        '^@cennznet/util(.*)': './packages/util/src\\1',
       }
     }]
   ]
