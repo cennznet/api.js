9d130bf98f5cc6bcacdf037fa9374f1ebf5b35e8
KarishmaBothara <bothara.karishma@gmail.com>
2021-08-30 15:09:27 +1200

    Updated contracts, txHash has changed for test

--- packages/api/test/e2e/ethBridge.e2e.ts
+++ packages/api/test/e2e/ethBridge.e2e.ts
@@ -29,10 +29,11 @@ describe('Eth bridge test', () => {
 
   describe('Eth bridge claims', () => {
   it('Submit claim for test token 1 from BridgeTest account', async done => {
-    const depositTxHash = "0xcf38ab5a958c962d286eff3b487ca44da01e42ddb9d9848327dec1e14d39329d";
+    // const depositTxHash = "0x3669266003c569752d4efcd4d63b9f9fac7d35a48e79490e74afe4f2b34b9231";
+    const depositTxHash = "0x1c8fdcfeec5a5cb0ec6a5329d4192021a4deae6fe650186c8d69a1037d20da97";
     testTokenId1 = await api.query.genericAsset.nextAssetId();
     const claim = {
-      tokenAddress: "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512",
+      tokenAddress: "0xcf7ed3acca5a467e9e704c703e8d87f634fb0fc9",
       amount: "1423",
       beneficiary: "0xacd6118e217e552ba801f7aa8a934ea6a300a5b394e7c3f42cd9d6dd9a457c10"
     };
@@ -56,9 +57,10 @@ describe('Eth bridge test', () => {
   });
 
   it('Submit claim for test token 2 from Alice', async done => {
-    const depositTxHash = "0x040467352a3521aff88e7c9d3f5b02409cd23d21201da79dced1e93380a1f9fe";
+    // const depositTxHash = "0xe1ddb6a283f0204c12c8a88941fa2a97d379af91d983366df9aa69e106b265ad";
+    const depositTxHash = "0x7cfb08e472ee6133a14f11d5252e3c16b502b8b2c4a21c9bff063ef2c9ebed45";
     const claim = {
-      tokenAddress: "0x9fE46736679d2D9a65F0992F2272dE9f3c7fa6e0",
+      tokenAddress: "0xdc64a140aa3e981100a9beca4e685f962f0cf6c9",
       amount: "5644",
       beneficiary: "0xd43593c715fdd31c61141abd04a99fd6822c8558854ccde39a5684e7a56da27d"
     };
@@ -79,9 +81,10 @@ describe('Eth bridge test', () => {
   });
 
   it('Submit claim for test token 2 from Bob', async done => {
-    const depositTxHash = "0x465219300b281bd1c94fa89c9e1d64b6a74b1ea27547f2e37e0f4507d9d5685b";
+    // const depositTxHash = "0x8b845836e8351e2de7040d80d4ddea6f8d4ecceb5a2784cc53c212cbc776ab6d";
+    const depositTxHash = "0x0cc6d22c6dee5abfb5fa0ae05ea036dd143ee6418adc979b886f701345afaf9c";
     const claim = {
-      tokenAddress: "0x9fE46736679d2D9a65F0992F2272dE9f3c7fa6e0",
+      tokenAddress: "0xdc64a140aa3e981100a9beca4e685f962f0cf6c9",
       amount: "11644",
       beneficiary: "0x8eaf04151687736326c9fea17e25fc5287613693c912909cb226aa4794f26a48"
     };
