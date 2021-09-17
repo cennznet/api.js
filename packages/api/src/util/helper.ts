d69cf15c8bdfbe10334d6aae4f44dff258110959
KarishmaBothara <bothara.karishma@gmail.com>
2021-09-17 12:49:21 +1200

    fix lint issue

--- packages/api/src/util/helper.ts
+++ packages/api/src/util/helper.ts
@@ -36,51 +36,56 @@ export function extractEthereumSignature(signatures: EthereumSignature[]): { r: 
 export async function awaitDepositClaim(
   api: Api,
   depositTxHash: H256 | string | Uint8Array,
-  claim: Erc20DepositEvent | { tokenAddress?: any; amount?: any; beneficiary?: any } | string | Uint8Array,
+  claim: Erc20DepositEvent,
   signer: KeyringPair
 ): Promise<ClaimDeposited> {
   const nonce = await api.rpc.system.accountNextIndex(signer.address);
   return new Promise((resolve, reject) => {
-    api.tx.erc20Peg.depositClaim(depositTxHash, claim).signAndSend(signer, { nonce }, ({ status, events }) => {
-      let eventClaimId;
-      if (status.isInBlock) {
-        for (const {
-          event: { method, section, data },
-        } of events) {
-          if (section === 'erc20Peg' && method === 'Erc20Claim') {
-            eventClaimId = data[0];
-            break;
-          } else if (section === 'system' && method === 'ExtrinsicFailed') {
-            reject('Claim already notarized');
+    api.tx.erc20Peg
+      .depositClaim(depositTxHash, claim)
+      .signAndSend(signer, { nonce }, ({ status, events }) => {
+        let eventClaimId;
+        if (status.isInBlock) {
+          for (const {
+            event: { method, section, data },
+          } of events) {
+            if (section === 'erc20Peg' && method === 'Erc20Claim') {
+              eventClaimId = data[0];
+              break;
+            } else if (section === 'system' && method === 'ExtrinsicFailed') {
+              reject('Claim already notarized');
+            }
           }
         }
-      }
-      if (eventClaimId) {
-        api.query.system.events((events) => {
-          events.forEach((record) => {
-            const { event } = record;
-            if (event.section === 'erc20Peg' && event.method === 'Erc20Deposit') {
-              const [claimId] = event.data;
-              if (claimId.toString() === eventClaimId.toString()) {
-                console.debug('Deposit claim on CENNZnet side succeeded..');
-                resolve({
-                  claimId: (event.data[0] as u64).toNumber(),
-                  assetId: event.data[1].toString(),
-                  amount: event.data[2].toString(),
-                  beneficiary: event.data[3].toString(),
-                });
-              }
-            } else if (event.section === 'erc20Peg' && event.method === 'Erc20DepositFail') {
-              const [claimId] = event.data;
-              if (claimId.toString() === eventClaimId.toString()) {
-                console.error('Deposited claim on CENNZnet side failed..');
-                reject('Claim deposition failed');
-              }
-            }
-          });
-        });
-      }
-    });
+        if (eventClaimId) {
+          api.query.system
+            .events((events) => {
+              events.forEach((record) => {
+                const { event } = record;
+                if (event.section === 'erc20Peg' && event.method === 'Erc20Deposit') {
+                  const [claimId] = event.data;
+                  if (claimId.toString() === eventClaimId.toString()) {
+                    console.debug('Deposit claim on CENNZnet side succeeded..');
+                    resolve({
+                      claimId: (event.data[0] as u64).toNumber(),
+                      assetId: event.data[1].toString(),
+                      amount: event.data[2].toString(),
+                      beneficiary: event.data[3].toString(),
+                    });
+                  }
+                } else if (event.section === 'erc20Peg' && event.method === 'Erc20DepositFail') {
+                  const [claimId] = event.data;
+                  if (claimId.toString() === eventClaimId.toString()) {
+                    console.error('Deposited claim on CENNZnet side failed..');
+                    reject('Claim deposition failed');
+                  }
+                }
+              });
+            })
+            .catch((err) => console.log(err));
+        }
+      })
+      .catch((err) => console.log(err));
   });
 }
 
