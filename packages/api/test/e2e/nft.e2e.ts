ae8082d5a6d38bf4a00d06f52f3646929123d38c
KarishmaBothara <bothara.karishma@gmail.com>
2022-03-09 12:15:45 +1300

    as per review comment

--- packages/api/test/e2e/nft.e2e.ts
+++ packages/api/test/e2e/nft.e2e.ts
@@ -485,6 +485,12 @@ describe('NFTs', () => {
   it('Find tokens listing on local with V2', async done => {
     const allTokens = await api.derive.nft.openCollectionListingsV2('15');
     expect(allTokens.length).toBe(2);
+    expect(allTokens[0].listingId).toBe('3');
+    expect(allTokens[0].tokens[0].owner).toBe(tokenOwner.address);
+    expect(allTokens[0].tokens[0].tokenId.toString()).toBe(new EnhancedTokenId(api.registry, [15,0,1]).toString());
+    expect(allTokens[1].listingId).toBe('2');
+    expect(allTokens[1].tokens[0].owner).toBe(tokenOwner.address);
+    expect(allTokens[1].tokens[0].tokenId.toString()).toBe(new EnhancedTokenId(api.registry, [15,0,0]).toString());
     done();
   });
 
