58bed40cfaeed82a6a33ceb55c1b32caf9fdc257
KarishmaBothara <bothara.karishma@gmail.com>
2022-04-28 09:52:00 +1200

    add types

--- packages/types/src/utils.ts
+++ packages/types/src/utils.ts
@@ -1,7 +1,7 @@
 import {hexToU8a, stringToU8a, u8aConcat} from "@polkadot/util";
 import {encodeAddress} from "@polkadot/util-crypto";
 
-export function cvmToAddress(cvmAddress) {
+export function cvmToAddress(cvmAddress: string): string {
     let message = stringToU8a('cvm:');
     message = u8aConcat(message, new Array(7).fill(0), hexToU8a(cvmAddress));
     const checkSum = message.reduce((a, b) => a ^ b, 0);
